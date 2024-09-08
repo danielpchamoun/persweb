d5ba1b1cfba3362a5ed69e02f719839bdbf2f35d
Daniel Chamoun <chamoun2@illinois.edu>
2024-09-08 02:26:53 -0500

    update home page

--- myscripts.js
+++ myscripts.js
@@ -59,22 +59,71 @@ var intervalId = window.setInterval(function(){
 function selectAbout(){
     aboutSelected = 1;
     document.getElementById('aboutSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = `me
+
+
+
+
+
+
+
+ ___ 
+/. .\\
+\\_-_/
+ /T\\ 
+d | b
+ / \\ 
+
+
+
+
+
+
+
+
+
+`;
 
 }
 
 function selectProjects(){
     projectsSelected = 1;
     document.getElementById('projectsSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = `                                  embedded software
+    
+                ____________________________    .--. 
+               /                           /\\  | .-.|                __   
+              /                           / /\\ |T  ||            ___/  \\
+             /                          / \\/  [_|__|_]       ___/       \\
+            /                           /\\      |  |     ___/            \\          
+           /___________________________/ /      |  | ___/                 \\   
+          \\___________________________\\/         ___/                       \\ 
+           \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\     ___/                       ___/|
+                   .--.......--.        ____/                       ___/ _  (
+                .-(   |||| ||   )-.    /                        ___/ _   \\\\ |
+               \/   '--'''''''--'   \\   |\\  __               ___/ _   \\\\  _H_/
+                  .-.      _____       | \\/  \\          ___/ _   \\\\  _H_/ Y
+                 ( ( )  .-(_|||_)-.    |\`|  _/      ___/ _   \\\\  _H_/ Y   !   
+                 | |-|  '         '     \\|_|\\   ___/ _   \\\\  _H_/ Y   !   !
+                 | |=|  .  _____  .     !  | \\_/ _   \\\\  _H_/ Y   !   !
+                 | (_)  '-(_|||_)-'     !  \\\` |  \\\\  _H_/ Y   !   !
+                 |  |                       \\\`|  _H_/ Y   !   !
+                                             \\|_/ Y   !   !
+                                                  !   !
+                                                  !	
+`;
 
 }
 function selectSkills(){
     skillsSelected = 1;
     document.getElementById('skillsSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = "languages and frameworks";
 
 }
 function selectResume(){
     resumeSelected = 1;
     document.getElementById('resumeSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = "experience and work history";
 
 }
 
@@ -82,30 +131,36 @@ function selectResume(){
 function selectGrind(){
     grindSelected = 1;
     document.getElementById('grindSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = "my weekly stats";
 
 }
 
 function selectGithub(){
     githubSelected = 1;
     document.getElementById('githubSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = "see my code";
 
 }
 function selectChat(){
     chatSelected = 1;
     document.getElementById('chatSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = "internet checkpoint";
+
 
 }
 function selectBlog(){
     blogSelected = 1;
     document.getElementById('blogSelected').style.setProperty('--styleNum', styleVal2);
+    document.getElementById('ascii-scene').innerHTML = "project and life updates";
 
 }
+/*
 function selectSciAdv(){
     sciAdvSelected = 1;
     document.getElementById('sciAdvSelected').style.setProperty('--styleNum', styleVal2);
 
 }
-
+*/
 
 
 function resetSelected(){
@@ -114,7 +169,7 @@ function resetSelected(){
     skillsSelected = 0;
     resumeSelected = 0;
     grindSelected = 0;
-    sciAdvSelected = 0;
+    //sciAdvSelected = 0;
     blogSelected = 0;
     chatSelected = 0;
     githubSelected = 0;
@@ -124,9 +179,33 @@ function resetSelected(){
     document.getElementById('skillsSelected').style.setProperty('--styleNum', styleVal1);
     document.getElementById('resumeSelected').style.setProperty('--styleNum', styleVal1);
     document.getElementById('grindSelected').style.setProperty('--styleNum', styleVal1);
-    document.getElementById('sciAdvSelected').style.setProperty('--styleNum', styleVal1);
+    //document.getElementById('sciAdvSelected').style.setProperty('--styleNum', styleVal1);
     document.getElementById('blogSelected').style.setProperty('--styleNum', styleVal1);
     document.getElementById('chatSelected').style.setProperty('--styleNum', styleVal1);
     document.getElementById('githubSelected').style.setProperty('--styleNum', styleVal1);
+    document.getElementById('ascii-scene').innerHTML = `
+
+
+
+
+
+
+
+ ___   ___   ___   ___   ___   ___   ___   ___   ___
+/. .\\ /. .\\ /. .\\ /. .\\ /. .\\ /. .\\ /. .\\ /. .\\ /. .\\
+\\_-_/ \\_-_/ \\_-_/ \\_-_/ \\_-_/ \\_-_/ \\_-_/ \\_-_/ \\_-_/
+ /T\\   /T\\   /T\\   /T\\   /T\\   /T\\   /T\\   /T\\   /T\\ 
+d | b d | b d | b d | b d | b d | b d | b d | b d | b
+ / \\   / \\   / \\   / \\   / \\   / \\   / \\   / \\   / \\  
+
     
+
+
+
+
+
+
+     
+    `;
+
 }
